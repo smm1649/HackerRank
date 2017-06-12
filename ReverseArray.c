0011aac4a01b71c2ecccdf83b95208908eb501b0
sandeep munavalli <sandeep.munavalli@mavs.uta.edu>
2017-06-11 20:22:36 -0500

    ReverseArray.c
    Prints the Given array in the Reverse order

---
+++ ReverseArray.c
@@ -0,0 +1,22 @@
+\* Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers. */
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>}``
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+     scanf("%d",&n);
+    int *arr = malloc(sizeof(int) * n);    
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       scanf("%d",&arr[arr_i]);
+    }
+    for(int arr_i=n-1;arr_i>=0;arr_i--){
+        printf("%d ",arr[arr_i]);
+  }
+    return 0;
+}
